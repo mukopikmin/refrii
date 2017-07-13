c2c3d1ac594edc9a945ed72803b313e5dad1392c
Yusuke Mukai <opposite0302@gmail.com>
2017-07-14 00:23:51 +0900

    change domain

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -ex
 
 npm install
 npm install -g @angular/cli
-ng build -e prod --base-href https://mukopikmin.github.io/refrii/
+ng build -e prod --base-href https://refrii.com/
 cp dist/index.html dist/404.html
+echo refrii.com > dist/CNAME
